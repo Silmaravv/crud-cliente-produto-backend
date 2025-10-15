1fe7b5f8f76b2de81c787b4b39e0be9efeecebb2
AlexeyFrens <leiloka46@gmail.com>
2025-10-14 21:13:09 -0300

    chore: Configuracoes iniciais

---
+++ src/main/java/br/senac/tads/dsw/crudclienteprodutobackend/CrudClienteProdutoBackendApplication.java
@@ -0,0 +1,13 @@
+package br.senac.tads.dsw.crudclienteprodutobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClienteProdutoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClienteProdutoBackendApplication.class, args);
+	}
+
+}
